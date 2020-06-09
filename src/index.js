4a4cef03badd153e89262f8342d49537bc54d4f8
diegoenrique92 <diegoenrique92@gmail.com>
2020-06-09 00:24:55 -0500

    ESLint

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM, { version } from 'react-dom';
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
